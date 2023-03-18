c2b16c88c5b9688e457ba4f25bd711cbeb508c3a
thymac <thywillberdone@gmail.com>
2023-03-18 03:26:42 -0700

    revised

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,15 @@
-CREATE USER 'monty'@'localhost' IDENTIFIED BY 'some_pass';
-GRANT ALL PRIVILEGES ON *.* TO 'monty'@'localhost' WITH GRANT OPTION;
-CREATE USER 'monty'@'%' IDENTIFIED BY 'some_pass';
-GRANT ALL PRIVILEGES ON *.* TO 'monty'@'%' WITH GRANT OPTION;
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+	IF NOT EXISTS 'user_0d_1'@'localhost' 
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.*
+	TO 'user_0d_1'@'localhost'
+       	WITH GRANT OPTION;
+CREATE USER
+	'user_0d_1'@'%' 
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.*
+	TO 'user_0d_1'@'%'
+       	WITH GRANT OPTION;
