7f59de68f19e06c1f6043898f2028421cb090576
thymac <69418243+thymac@users.noreply.github.com>
2023-03-18 14:17:37 +0300

    revised

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,5 +4,6 @@ CREATE USER
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
    ON *.*
-   TO 'user_0d_1'@'localhost';
+   TO 'user_0d_1'@'localhost'
+   WITH GRANT OPTION;
 FLUSH PRIVILEGES;
