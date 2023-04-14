1f1f9e2926cb2fcd3cbc8dcc725c03fe9c5b31a3
thymac <thywillberdone@gmail.com>
2023-04-14 01:21:33 -0700

    revised

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+function factorial(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else if (Number.isNaN(num)|| num < 0) {
+    return 1;
+  } else {
+  return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(num));
