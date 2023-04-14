a7bb5713f03dc784bd1764a82156fca80ff77d44
thymac <thywillberdone@gmail.com>
2023-04-14 01:20:07 -0700

    revised

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const [arg0, arg1] = process.argv.slice(2);
+
+if (!arg0 && !arg1) {
+  console.log("undefined is undefined");
+} else if (!arg0) {
+  console.log("undefined is " + arg1);
+} else if (!arg1) {
+  console.log(arg0 + " is undefined");
+} else {
+  console.log(arg0 + " is " + arg1);
+}
