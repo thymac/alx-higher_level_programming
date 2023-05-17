a60afb9f80481e5c4f3ab15dd279a7eb03a773f6
thymac <thywillberdone@gmail.com>
2023-05-17 03:58:12 -0700

    revised

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,33 +1,13 @@
 #!/usr/bin/python3
-"""
-Module: 0-hbtn_status
-Fetches https://alx-intranet.hbtn.io/status and displays the response body.
-
-"""
-
+"Module that fetches https://alx-intranet.hbtn.io/status"
 import urllib.request
-
-def fetch_status():
-    """
-    Fetches the status from https://alx-intranet.hbtn.io/status.
-    
-    Returns:
-        A string representing the response body.
-    """
-    url = 'https://alx-intranet.hbtn.io/status'
-
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-
-    return body.decode('utf-8')
 
 
 if __name__ == "__main__":
-    """
-    If this module is executed directly, fetches and displays the status.
-    """
-    print("Body response:")
-    print("\t- type:", type(fetch_status()))
-    print("\t- content:", fetch_status())
-    print("\t- utf8 content:", fetch_status())
-
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
