639616d36ed0a1674c43f5f0d40225fe6b6da021
thymac <thywillberdone@gmail.com>
2023-05-23 18:24:19 +0000

    revised

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -10,8 +10,8 @@ const process = require('process');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+fs.writeFile(filePath, content, 'utf-8', function (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   }
 });
