44944d0dd5ed7afb8f730d95f30a6372e89cf163
thymac <thywillberdone@gmail.com>
2023-05-23 18:20:16 +0000

    revised

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-/*
-A script that writes a string to a file.
-The first argument is the file path
-The second argument is the string to write
-The content of the file must be written in utf-8
-If an error occurred during while writing, print the error object
-*/
+// A script that writes a string to a file.
+// The first argument is the file path
+// The second argument is the string to write
+// The content of the file must be written in utf-8
+// If an error occurred during while writing, print the error object
 
 const fs = require('fs');
 const process = require('process');
