271bfcbd72a1dcd457511f5efe41fec227569220
thymac <thywillberdone@gmail.com>
2023-06-03 08:02:51 -0700

    revised

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -17,8 +17,8 @@ def find_peak(list_of_integers):
     while low < high:
         mid = (low + high) // 2
 
-        if list_of_integers[mid] > list_of_integers[mid - 1]
-        and list_of_integers[mid] > list_of_integers[mid + 1]:
+        if (list_of_integers[mid] > list_of_integers[mid - 1]
+                and list_of_integers[mid] > list_of_integers[mid + 1]):
             return list_of_integers[mid]
         elif list_of_integers[mid] < list_of_integers[mid + 1]:
             low = mid + 1
