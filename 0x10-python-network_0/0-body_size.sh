c7c6a21d2ff3f4f21d1818f4fae2247f48fb1765
thymac <thywillberdone@gmail.com>
2023-06-04 05:44:22 -0700

    revised

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# A script that takes a URL, sends a GET request, displays the response body
-curl - sI "$1" | grep - i Content - Length | awk '{print $2}'
+# A script that takes a URL as an argument, sends a GET request to that URL, and displays the response body
+
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
