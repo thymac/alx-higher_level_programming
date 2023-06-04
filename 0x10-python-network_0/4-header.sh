5623a256d5a9cf7f406b10784eca96a476b40da0
thymac <thywillberdone@gmail.com>
2023-06-04 06:44:10 -0700

    revised

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A script sends a GET request with a custom header, displays the response body
-curl -s "$1" -H "X-School-User-Id: 98"
-
+# Sends a GET request with a specific header and displays the response body
+curl -sH "X-School-User-Id: 98" "$1"
