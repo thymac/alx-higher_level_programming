d65c6c21e6de76a0af4664ed1182ca1e9b420bfa
thymac <thywillberdone@gmail.com>
2023-06-04 06:21:50 -0700

    revised

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a POST request with specified parameters and displays the response body
-curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
-
+curl -sX POST -d"email=test@gmail.com&subject=I will always be here for PLD" $1
